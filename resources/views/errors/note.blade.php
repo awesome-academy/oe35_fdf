c54f12fd4490040e98dbdbf10923b6285119714a
HUYNH HUY HOANG <huynhhuyhoang9999@gmail.com>
2020-08-24 15:38:00 +0700

    fixx manage users

--- resources/views/errors/note.blade.php
+++ resources/views/errors/note.blade.php
@@ -1,22 +1,6 @@
 @if(Session()->has('error'))
-<<<<<<< HEAD
 	<p class="alert alert-danger">{{Session::get('error')}}</p>
 @endif
-
-
 @foreach($errors->all() as $error)
 	<p class="alert alert-danger">{{$error}}</p>
 @endforeach
-=======
-
-	<p class="alert alert-danger">{{Session::get('error')}}</p>
-
-@endif
-
-@foreach($errors->all() as $error)
-
-	<p class="alert alert-danger">{{$error}}</p>
-
-@endforeach
-
->>>>>>> 7a79e16... Manager Categories
