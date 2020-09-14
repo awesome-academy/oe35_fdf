fa366a9e6ab25d6f6105a59405fb65fea135af6c
Huan200799 <55988824+Huan200799@users.noreply.github.com>
2020-09-14 09:55:22 +0700

    Thong Ke

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
-use App\Models\User;
+use App\User;
 use App\Models\Order;
 use App\Models\OrderDetail;
 use Illuminate\Support\Facades\Redirect;
 use Cart;
-
+use Mail;
 use Illuminate\Http\Request;
-
+use App\Jobs\SendMailAdmin;
 class OrderController extends Controller
 {
     public function place_order()
@@ -35,15 +35,15 @@ class OrderController extends Controller
                 $orderdetail->order_product_name = $datum->name;
                 $orderdetail->order_product_totalprice = ($datum->qty * $datum->price);
                 $orderdetail->quantity = $datum->qty;
-
                 $orderdetail->save();
                 Cart::destroy();
-
                 $orderdetail = new OrderDetail();
             };
             $order->user_id = Auth::user()->id;
             $order->total_price = $total;
             $order->save();
+            $email = new SendMailAdmin();
+            dispatch($email);
 
             return Redirect::route('showcart');
         }else {
